3eb8df11d47ff4945d6bc8634794beb8f21a498b
Adam Carlisle <40108251@qub.ac.uk>
2017-03-06 18:37:49 +0000

    Implemented suggested changes to SDES

--- src/iss7/encryption/SDES.java
+++ src/iss7/encryption/SDES.java
@@ -1,26 +1,22 @@
-package encryption;
 
 public class SDES {
-
-	private static String sourceKey = "1111011000";
-	private static String plaintext;
-	private static String key1, key2, p10Transformed, ciphertext;
-	private static String temp1,leftBits, rightBits, fResult,fLeft,fRight;
-	private static StringBuilder sbCipher = new StringBuilder();
-	private static int[] p10 = {3,5,2,7,4,10,1,9,8,6};
-	private static int[] p8 = {6,3,7,4,8,5,10,9};
-	private static int[] p4 = {2,4,3,1};
-	private static int[] ip = {2,6,3,1,4,8,5,7};
-	private static int[] ep = {4,1,2,3,2,3,4,1};
-	private static int[] inverseP = {4,1,3,5,7,2,8,6};
-	private static char tempChar;
-	private static int[][] sBox0 = {
+	
+	/**
+	 * Permutation and Substitution arrays
+	 */	
+	private static final int[] P10 = {3,5,2,7,4,10,1,9,8,6};
+	private static final int[] P8 = {6,3,7,4,8,5,10,9};
+	private static final int[] P4 = {2,4,3,1};
+	private static final int[] IP = {2,6,3,1,4,8,5,7};
+	private static final int[] EP = {4,1,2,3,2,3,4,1};
+	private static final int[] INVERSE_P = {4,1,3,5,7,2,8,6};
+	private static final int[][] S_BOX_0 = {
 		{1,0,3,2},
 		{3,2,1,0},
 		{0,2,1,3},
 		{3,1,3,2},
 		};
-	private static int[][] sBox1 = {
+	private static final int[][] S_BOX_1 = {
 		{0,1,2,3},
 		{2,0,1,3},
 		{3,0,1,0},
@@ -28,18 +24,24 @@ public class SDES {
 		};
 		
 	/**
-	 * Empty constructor
+	 * Empty constructor to prevent instantiation
 	 */
 	private SDES(){}
 	
+	
 	/**
-	 * Public method that calls the private Encrypt method
-	 * @param enteredText The plaintext to be encrypted
-	 * @return ciphertext
+	 * This class is used to store the keys used in encryption
+	 * having it as an inner class rather than a global variable
+	 * is more secure as the keys live/die in the class and are not 
+	 * visible on the stack.
+	 *
 	 */
-	public static String Encrypt(String enteredText){
-		plaintext = enteredText;
-		return Encrypt();
+	private static class KeyWrapper{
+		private String key1,key2;
+		private KeyWrapper(String k1, String k2){
+			this.key1 = k1;
+			this.key2 = k2;
+		}
 	}
 	
 	/**
@@ -48,9 +50,11 @@ public class SDES {
 	 * @param bits String you want to shift left
 	 * @return The shifted string
 	 */
-	private static String ShiftLeft(int num, String bits){
+	private static String shiftLeft(int num, String bits){
 		assert(bits.length() == 10);
-		temp1 = bits;
+		String temp1 = bits;
+		String leftBits,rightBits;
+		char tempChar;
 		for(int x = 0; x < num; x++){
 			tempChar = temp1.charAt(0);
 			leftBits = temp1.substring(1, 5) + tempChar;
@@ -62,75 +66,76 @@ public class SDES {
 		return temp1;
 	}
 	
+	
 	/**
-	 * This method creates Key 1 and Key 2 from the 10 bit source key
+	 * This method uses the 10 bit source key to generate two
+	 * 8 bit sub keys to be used in the encryption method.
+	 * @return The Keywrapper object with the two keys
 	 */
-	private static void GenerateKeys(){
+	private static KeyWrapper generateKeys(){
+		String p10Transformed;
+		String key1,key2;
+		final String SOURCE_KEY = "1111011000"; 
 		//KEY 1
-		p10Transformed = Permutation(p10,sourceKey);
-		key1 = ShiftLeft(1,p10Transformed);
-		key1 = Permutation(p8,key1);
+		p10Transformed = permutation(P10,SOURCE_KEY);
+		key1 = shiftLeft(1,p10Transformed);
+		key1 = permutation(P8,key1);
 		
 		//KEY 2
-		key2 = ShiftLeft(3,p10Transformed);
-		key2 = Permutation(p8,key2);
+		key2 = shiftLeft(3,p10Transformed);
+		key2 = permutation(P8,key2);
+		
+		return new KeyWrapper(key1,key2);
 	}
 	
 	/**
-	 * This method first call "GenerateKeys" to allow for encryption
+	 * This method first call "GenerateKeys" to retrieve required keys for encryption
 	 * The plaintext is converted to its ASCII representation and stored in a byte array
 	 * The ASCII code is then broken down into groups of 8 and converted to its binary representation
 	 * 0s are added if padding is necessary then stored in a String array.
 	 * Each element in the binary array is then modified by P10 Permutation, FFunction with Key 1,
 	 * the first four and last four bits are then swapped, put through the FFunction with Key 2
 	 * and then finally Inverse Permutation is applied.
-	 * Each iteration gets added to the StringBuilder cipher and gets converted to a String and stored.	 * 
+	 * Each iteration gets added to the StringBuilder cipher and gets converted to a String and stored.
+	 * @param plaintext String to be encrypted
 	 * @return The ciphertext
 	 */
-	private static String Encrypt(){
-		assert(!(plaintext == null));
-		GenerateKeys();
+	public static String encrypt(String plaintext){
+		assert(plaintext != null);
+		StringBuilder sbCipher = new StringBuilder();
+		KeyWrapper keys = generateKeys();
 		//Breaking text into Ascii then getting the binary version
 		//Adding 0s if it's not 8 bits long.
 		byte[] ascii = plaintext.getBytes();
 		String[] binary = new String[ascii.length];
-		int binLength;
 		for(int x = 0; x < ascii.length; x++){
 			binary[x] = Integer.toBinaryString(ascii[x]);
-			if(binary[x].length() < 8){
-				binLength = 8 - binary[x].length();
-				for(int y = 0; y < binLength; y++){
-					binary[x] = "0" + binary[x];
-				}
-			}
+			//Padding
+			binary[x] = "00000000".substring(binary[x].length()) + binary[x];
 		}		
 		for(int x = 0; x < binary.length; x++){	
 			//IP
-			binary[x] = Permutation(ip,binary[x]);
+			binary[x] = permutation(IP,binary[x]);
 			//F with Key 1
-			binary[x] = FFunction(binary[x], key1);
+			binary[x] = fFunction(binary[x], keys.key1);
 			//Swap
-			binary[x] = Swap8(binary[x]);
+			binary[x] = swap8(binary[x]);
 			//F with Key 2
-			binary[x] = FFunction(binary[x], key2);
+			binary[x] = fFunction(binary[x], keys.key2);
 			//Inverse P
-			binary[x] = Permutation(inverseP,binary[x]);
+			binary[x] = permutation(INVERSE_P,binary[x]);
 			//Add to ciphertext
 			sbCipher.append(binary[x]);
 		}
-		ciphertext = sbCipher.toString();
-		sbCipher.delete(0, sbCipher.length());
-		return ciphertext;
+		return sbCipher.toString();
 	}
-	
-
 	
 	/**
 	 * This method swaps the first four and the last four bits
 	 * @param bits The String of bits you want swapped
 	 * @return swapped bits string
 	 */
-	private static String Swap8(String bits){
+	private static String swap8(String bits){
 		assert(bits.length() == 8);
 		return bits.substring(4, 8) + bits.substring(0, 4);
 	}
@@ -146,7 +151,7 @@ public class SDES {
 	 * @param bits The bits to be S-Box transformed
 	 * @return Transformed bit String
 	 */
-	private static String SBOXTransform(int[][] sBox, String bits){
+	private static String sBOXTransform(int[][] sBox, String bits){
 		
 		assert(bits.length() == 4);
 		StringBuilder sb = new StringBuilder();
@@ -177,7 +182,7 @@ public class SDES {
 	 * @param bits The string to be permuted
 	 * @return Permuted String
 	 */
-	private static String Permutation(int[] p, String bits){
+	private static String permutation(int[] p, String bits){
 		assert(!(p==null) && !(bits==null));
 		StringBuilder sb = new StringBuilder();
 		for(int x = 0; x < p.length; x++){
@@ -193,7 +198,7 @@ public class SDES {
 	 * @param two Second String to be XOR'd
 	 * @return XOR'd result
 	 */
-	private static String XOR(String one, String two){
+	private static String xor(String one, String two){
 		assert(one.length() == two.length());
 		StringBuilder xor = new StringBuilder();
 		for(int y = 0; y < one.length(); y++){
@@ -216,37 +221,40 @@ public class SDES {
 	 * @param key The key used for the XOR function.
 	 * @return String of bits that have been modified by the FFunction
 	 */
-	private static String FFunction(String binary, String key){
+	private static String fFunction(String binary, String key){
 		assert(binary.length() == 8);
+		String fResult, fLeft,fRight,leftBits,rightBits;
 		//Split 8 bits into left and right
 		leftBits = binary.substring(0, 4);
 		rightBits = binary.substring(4, 8);
 		
 		//EP on right bits
-		fResult = Permutation(ep,rightBits);
+		fResult = permutation(EP,rightBits);
 		
 		//XOR Key + EP Transformed right bits
-		fResult = XOR(fResult,key);
+		fResult = xor(fResult,key);
 		
 		//sBox transforms
 		fLeft = fResult.substring(0, 4);
 		fRight = fResult.substring(4, 8);
-		fLeft = SBOXTransform(sBox0,fLeft);		
-		fRight = SBOXTransform(sBox1,fRight);
+		fLeft = sBOXTransform(S_BOX_0,fLeft);		
+		fRight = sBOXTransform(S_BOX_1,fRight);
 		fResult = fLeft + fRight;
 		
 		//P4 Transform
-		fResult = Permutation(p4,fResult);
+		fResult = permutation(P4,fResult);
 		
 		//XOR with Left bits + FResult
-		return XOR(leftBits,fResult) + rightBits;
+		return xor(leftBits,fResult) + rightBits;
 	}
 	
 	public static void main(String[] args) {
 		String testAns = "10001111";
-		String cipher = (Encrypt("T"));
-		assert(testAns == cipher);
+		String cipher = (encrypt("T"));
+		System.out.println(cipher);
+		assert(testAns.equals(cipher));
 		
 	}
-
 }
+
+
