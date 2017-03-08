6f78c42c17a5f7a9fd1135ace41d3a0070b3d844
Chris McKee <cmckee41@qub.ac.uk>
2017-03-08 11:53:45 +0000

    Encrypt and decrypt both working in block form
    Known bug involved converting encrypted block to String, then trying
    to decrypt said string. Probably something to do with casting ints
    to bytes, or ascii range.

--- src/iss7/encryption/AES.java
+++ src/iss7/encryption/AES.java
@@ -1,5 +1,6 @@
 package encryption;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.Logger;
 
@@ -40,14 +41,42 @@ public class AES {
             {0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf},
             {0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16}
     };
+
+    private static final int[][] INVERSE_S_BOX = {
+            {0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb},
+            {0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb},
+            {0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e},
+            {0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25},
+            {0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92},
+            {0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84},
+            {0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06},
+            {0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b},
+            {0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73},
+            {0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e},
+            {0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b},
+            {0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4},
+            {0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f},
+            {0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef},
+            {0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61},
+            {0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d}
+    };
+
     private static final int LEFT_MASK = 0xF0;
     private static final int RIGHT_MASK = 0x0F;
 
-    private static final String[][] MCT = {
-            {"02", "03", "01", "01"},
-            {"01", "02", "03", "01"},
-            {"01", "01", "02", "03"},
-            {"03", "01", "01", "02"}
+    // Arrays of operations for MCT and inverse
+    private static final int[][] MCT = {
+            {0x02, 0x03, 0x01, 0x01},
+            {0x01, 0x02, 0x03, 0x01},
+            {0x01, 0x01, 0x02, 0x03},
+            {0x03, 0x01, 0x01, 0x02}
+    };
+
+    private static final int[][] INVERSE_MCT = {
+            {0x0e, 0x0b, 0x0d, 0x09},
+            {0x09, 0x0e, 0x0b, 0x0d},
+            {0x0d, 0x09, 0x0e, 0x0b},
+            {0x0b, 0x0d, 0x09, 0x0e}
     };
 
     // Prevents instantiation
@@ -56,27 +85,14 @@ public class AES {
 
     public static String encrypt(String plaintext) {
         LOGGER.info("Entered plaintext: |" + plaintext + "|");
-        byte[] bytes = plaintext.getBytes();
-        LOGGER.info("Plaintext as ASCII values: " + Arrays.toString(bytes));
-        int[][] stateArray = new int[4][4];
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                stateArray[j][i] = bytes[4 * i + j];
-            }
-        }
+
+        int[][] stateArray = stringToBlock(plaintext);
         LOGGER.info("Plaintext as block: " + Arrays.deepToString(stateArray));
 
-        encrypt(stateArray);
+        stateArray = encrypt(stateArray);
+        LOGGER.info("Ciphertext as block: " + Arrays.deepToString(stateArray));
 
-        byte[] resultBytes = new byte[bytes.length];
-        for (int i = 0; i < stateArray.length; i++) {
-            for (int j = 0; j < stateArray[i].length; j++) {
-                resultBytes[4 * i + j] = (byte) stateArray[i][j];
-            }
-        }
-
-        String ciphertext = new String(resultBytes);
-
+        String ciphertext = blockToString(stateArray);
         LOGGER.info("Encrypted text: " + ciphertext);
 
         return ciphertext;
@@ -88,18 +104,12 @@ public class AES {
         stateArray = addRoundKey(stateArray, key);
 
         for (int i = 1; i <= NUMBER_ROUNDS; i++) {
-            LOGGER.info("Beginning round: " + (i));
             key = keyExpansion(key, i);
-            LOGGER.info("Before sub : " + i + "---" + Arrays.deepToString(stateArray));
             stateArray = substituteBytes(stateArray);
-            LOGGER.info("Before Shift : " + i + "---" + Arrays.deepToString(stateArray));
             stateArray = shiftRowsLeft(stateArray);
             if (i < NUMBER_ROUNDS) {
-                LOGGER.info("Before mix at round: " + i + "--" + Arrays.deepToString(stateArray));
-                stateArray = mixColumns(stateArray);
+                stateArray = forwardMixColumns(stateArray);
             }
-            LOGGER.info("Before Round : " + i + "---" + Arrays.deepToString(stateArray));
-
             stateArray = addRoundKey(stateArray, key);
         }
 
@@ -109,41 +119,59 @@ public class AES {
         return stateArray;
     }
 
+    public static String decrypt(String ciphertext) {
+        int[][] stateArray = stringToBlock(ciphertext);
+        printMatrixAsHex("Initial", stateArray);
+
+        stateArray = decrypt(stateArray);
+        printMatrixAsHex("Final", stateArray);
+
+        return blockToString(stateArray);
+
+    }
+
+    private static int[][] decrypt(int[][] state) {
+        int[][] key = KEY;
+        int[][][] allKeys = new int[10][4][4];
+        int[][] newState;
+        //Getting and storing all keys to be accessed later
+        for (int i = 1; i <= NUMBER_ROUNDS; i++) {
+            key = keyExpansion(key, i);
+            allKeys[i - 1] = key;
+        }
+        newState = addRoundKey(state, allKeys[9]);
+
+
+        for (int i = NUMBER_ROUNDS - 1; i > 0; i--) {
+            newState = shiftRowsRight(newState);
+            newState = reverseSubBytes(newState);
+            newState = addRoundKey(newState, allKeys[i - 1]);
+            newState = inverseMixColumns(newState);
+        }
+        newState = shiftRowsRight(newState);
+        newState = reverseSubBytes(newState);
+        newState = addRoundKey(newState, KEY);
+        LOGGER.info("Final result: " + Arrays.deepToString(newState));
+
+        return newState;
+    }
+
     private static int[][] substituteBytes(int[][] state) {
+        return sBoxLookup(state, S_BOX);
+    }
+
+    private static int[][] reverseSubBytes(int[][] state) {
+        return sBoxLookup(state, INVERSE_S_BOX);
+    }
+
+    private static int[][] sBoxLookup(int[][] state, int[][] transformation) {
         int[][] newState = new int[state.length][state[0].length];
         for (int i = 0; i < state.length; i++) {
             for (int j = 0; j < state[i].length; j++) {
                 // Use masks of F0 and 0F to separate row/column values
                 int row = (state[i][j] & LEFT_MASK) / 16;
                 int column = (state[i][j] & RIGHT_MASK);
-                newState[i][j] = S_BOX[row][column];
-            }
-        }
-        return newState;
-    }
-
-    private static int[][] reverseSubstitution(int[][] state){
-        int[][] newState = new int[state.length][state[0].length];
-        for (int i = 0; i < state.length; i++) {
-            for (int j = 0; j < state[i].length; j++){
-                //find the index of the element
-                int value = state[i][j];
-                //System.out.println(value);
-
-                for(int k = 0; k < S_BOX.length; k++){
-                    for(int l = 0; l < S_BOX.length; l++){
-                        if(S_BOX[k][l] == value){
-
-                            int first = k;
-                            int second = (l);
-
-                            String test =Integer.toHexString(first) + Integer.toHexString(second);
-                            System.out.println(test);
-                            newState[i][j] = Integer.parseInt(test,16) ;
-                        }
-                    }
-                }
-
+                newState[i][j] = transformation[row][column];
             }
         }
         return newState;
@@ -190,7 +218,15 @@ public class AES {
         return newRow;
     }
 
-    private static int[][] mixColumns(int[][] state) {
+    private static int[][] forwardMixColumns(int[][] state) {
+        return mixColumns(state, MCT);
+    }
+
+    private static int[][] inverseMixColumns(int[][] state) {
+        return mixColumns(state, INVERSE_MCT);
+    }
+
+    private static int[][] mixColumns(int[][] state, int[][] transformation) {
         int[][] newState = new int[state.length][state[0].length];
         for (int i = 0; i < state.length; i++) {
             for (int j = 0; j < state[i].length; j++) {
@@ -199,41 +235,58 @@ public class AES {
             }
         }
         return newState;
-
     }
 
     private static int[] columnToArray(int[][] state, int j) {
+        // Param j is the index of the column
         return new int[]{state[0][j], state[1][j], state[2][j], state[3][j]};
     }
 
-    private static int multiplyRowByColumn(String[] row, int[] column) {
+    private static int multiplyRowByColumn(int[] row, int[] column) {
         int[] results = new int[column.length];
         for (int i = 0; i < row.length; i++) {
-            results[i] = mixColumnValues(row[i], column[i]);
+            results[i] = finiteFieldMultiplication(row[i], column[i]);
         }
         return results[0] ^ results[1] ^ results[2] ^ results[3];
     }
 
-    private static int mixColumnValues(String operation, int stateValue) {
-        if (operation.equals("01")) {
-            return stateValue;
-        } else {
-            int returnValue;
-            // Seemed easier to use substrings here, I'm open to suggestions
-            // though
-            String asBinary = Integer.toBinaryString(stateValue);
-            String paddedBinary = "00000000".substring(asBinary.length()) + asBinary;
-            String shifted = paddedBinary.substring(1) + "0";
-            returnValue = Integer.parseInt(shifted, 2);
-            if (paddedBinary.charAt(0) == '1') {
-                returnValue = returnValue ^ 0x1b;
-            }
-            if (operation.equals("03")) {
-                returnValue = returnValue ^ stateValue;
-            }
+    /**
+     * Performs a multiplication of y by x, for an 8 bit number.
+     *
+     * @param x - the number of times by which to multiply y
+     * @param y - the number to be multiplied
+     * @return result xy
+     */
+    private static int finiteFieldMultiplication(int x, int y) {
+        int result = y;
 
-            return returnValue;
+        // First calculate the order in which to perform the operations upon y by reducing x to 1 by only
+        // halving it or subtracting 1. This gives the sequence of operations which need to be performed upon
+        // y to turn it from y into xy.
+        ArrayList<Integer> operations = new ArrayList<>();
+        int loopCounter = x;
+        while (loopCounter > 1) {
+            if (loopCounter % 2 != 0) {
+                loopCounter--;
+                operations.add(0, 1);
+            } else {
+                loopCounter = loopCounter / 2;
+                operations.add(0, 2);
+            }
         }
+
+        // Apply the operations in order, where 1 indicates that y should be added to the current number, while
+        // 2 indicates that the current number should be doubled.
+        for (int operation : operations) {
+            if (operation == 1) {
+                result = result ^ y;
+            } else if (operation == 2) {
+                result = result << 1;
+                if (result > 255)
+                    result = result & 0xff ^ 0x1b;
+            }
+        }
+        return result;
     }
 
     private static int[][] addRoundKey(int[][] state, int[][] key) {
@@ -254,9 +307,11 @@ public class AES {
 
         int rcon;
         if (numRounds < 9)
+            // 2^7 is the largest value that can be represented by 8 bits without having some funk
             rcon = 1 << (numRounds - 1);
         else {
             rcon = 1 << 8;
+            // Continue to double until target rcon is reached, wrapping any time the maximum value is exceeded.
             for (int i = 8; i < numRounds; i++) {
                 rcon = rcon << 1;
                 if (rcon > 255)
@@ -284,6 +339,13 @@ public class AES {
 
     }
 
+    /**
+     * Utility method for debugging. Provides an easy way to print a matrix to sysout in
+     * a human readable form, with a specified message.
+     *
+     * @param name  - string to print before matrix
+     * @param state - matrix to be printed
+     */
     private static void printMatrixAsHex(String name, int[][] state) {
         System.out.println(name + ": ");
         for (int[] i : state) {
@@ -294,31 +356,28 @@ public class AES {
         }
     }
 
-    public static String decrypt(int[][] state) {
-        // Lets do the inverse
-        int[][] key = KEY;
-        int[][][] allKeys = new int[10][4][4];
-        //Getting and storing all keys to be accessed later
-        for (int i = 1; i <= NUMBER_ROUNDS; i++) {
-           key=keyExpansion(key, i);
-           allKeys[i-1] = key;
+    private static int[][] stringToBlock(String text) {
+        byte[] bytes = text.getBytes();
+        int[][] stateArray = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                stateArray[j][i] = bytes[4 * i + j];
+            }
         }
-        int[][] lastRoundKey = addRoundKey(state, allKeys[9]);
-        LOGGER.info("Before Last Round: " + Arrays.deepToString(lastRoundKey));
+        return stateArray;
+    }
 
-        int[][] newState = shiftRowsRight(lastRoundKey);
-        LOGGER.info("Before final shift: " + Arrays.deepToString(newState));
+    private static String blockToString(int[][] stateArray) {
 
-        int[][] bSubState = reverseSubstitution(newState);
-        LOGGER.info("Before sub : " + Arrays.deepToString(bSubState));
-
-        for(int i = NUMBER_ROUNDS - 2; i>0; i--){
-            int[][] round = addRoundKey(bSubState, allKeys[i]);
-            LOGGER.info("Before  Round : " + (i+1)+ "---" + Arrays.deepToString(round));
-
+        byte[] resultBytes = new byte[16];
+        System.out.println(Arrays.deepToString(stateArray));
+        for (int i = 0; i < stateArray.length; i++) {
+            for (int j = 0; j < stateArray[i].length; j++) {
+                resultBytes[(4 * i) + j] = (byte) stateArray[i][j];
+            }
         }
-
-        return "";
+        System.out.println(Arrays.toString(resultBytes));
+        return new String(resultBytes);
     }
 
     public static void main(String[] args) {
@@ -346,16 +405,18 @@ public class AES {
         };
         int[][] resultShiftRows = shiftRowsLeft(testShiftRows);
         assert (Arrays.deepEquals(resultShiftRows, expectedShiftRows));
+        int[][] resultInvShiftRows = shiftRowsRight(resultShiftRows);
+        assert (Arrays.deepEquals(resultInvShiftRows, testShiftRows));
 
 
         // Test mixColumnValues
-        int testOneMix = 0x5d;
-        int testTwoMix = 0xd4;
-        int testThreeMix = 0xbf;
-        assert (mixColumnValues("01", testOneMix) == testOneMix);
-        assert (mixColumnValues("02", testTwoMix) == 0xb3);
-        assert (mixColumnValues("03", testThreeMix) == 0xda);
-        assert (mixColumnValues("03", 0x41) == 0xc3);
+        assert (finiteFieldMultiplication(0x01, 0x5d) == 0x5d);
+        assert (finiteFieldMultiplication(0x02, 0xd4) == 0xb3);
+        assert (finiteFieldMultiplication(0x03, 0xbf) == 0xda);
+        assert (finiteFieldMultiplication(0x03, 0x41) == 0xc3);
+        assert (multiplyRowByColumn(INVERSE_MCT[0], new int[]{0xe0, 0xcb, 0x19, 0x9a}) == 0xe0);
+        assert (multiplyRowByColumn(INVERSE_MCT[0], new int[]{0x04, 0x66, 0x81, 0xe5}) == 0xd4);
+
 
         // Test full MCT
         int[][] testMixColumns = {
@@ -365,7 +426,7 @@ public class AES {
                 {0x30, 0xae, 0xf1, 0xe5},
         };
 
-        int[][] resultMixColumns = mixColumns(testMixColumns);
+        int[][] resultMixColumns = forwardMixColumns(testMixColumns);
         int[][] expectedMixColumns = {
                 {0x04, 0xe0, 0x48, 0x28},
                 {0x66, 0xcb, 0xf8, 0x06},
@@ -373,6 +434,10 @@ public class AES {
                 {0xe5, 0x9a, 0x7a, 0x4c}
         };
         assert (Arrays.deepEquals(resultMixColumns, expectedMixColumns));
+
+        int[][] reverseMixColumns = inverseMixColumns(expectedMixColumns);
+        assert (Arrays.deepEquals(reverseMixColumns, testMixColumns));
+
 
         // Test add round key
 
@@ -411,6 +476,10 @@ public class AES {
         int[][] resultEncryptOutput = encrypt(testEncrypt);
         assert (Arrays.deepEquals(resultEncryptOutput, expectedEncryptOutput));
 
+        int[][] resultDecryptOutput = decrypt(resultEncryptOutput);
+        assert (Arrays.deepEquals(resultDecryptOutput, testEncrypt));
+
+
         String test;
         if (args.length == 0) {
             test = "mark frequency  ";
@@ -419,9 +488,9 @@ public class AES {
             test = args[0];
         }
         String ciphertext = encrypt(test);
-        //Hardcoded new state to be removed
-        int[][] newState = {{64, 38, 178, 31}, {56, 0, 56, 192}, {123, 179, 12, 195}, {179, 90, 70, 114}};
-        String plaintext = decrypt(newState);
-        //        assert (plaintext.equals(test));
+        String plaintext = decrypt(ciphertext);
+        System.out.println(plaintext);
+        // Commented out until we work out where the bug in blockToString / stringToBlock is
+        // assert (plaintext.equals(test));
     }
 }
