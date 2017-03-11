91fbd1fa9cea53a896564e41afb6bcd232589960
40107481 <dwalsh17@qub.ac.uk>
2017-03-11 21:44:28 +0000

    fixed key generations, encrypt + decrypt added

--- src/iss7/encryption/RSA.java
+++ src/iss7/encryption/RSA.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class RSA {
 
-    private static final String plaintext="markfrequency";
+    private static final String plaintext="abc";
     private static final int p = 41;
     private static final int q = 67;
     private static final int d = 83;
@@ -11,17 +11,28 @@ public class RSA {
 
     private static class Key{
         private int partOne,partTwo;
-        private Key(int f, int n){
-            this.partOne = f;
+        private Key(int d_e, int n){
+            this.partOne = d_e;
             this.partTwo = n;
         }
     }
 
-    private static Key genPublicKey(int d, int w){
+    private static class KeyValues{
+        private int e,d,n;
+        private KeyValues(int e, int d, int n){
+            this.e = e;
+            this.d = d;
+            this.n = n;
+        }
+    }
 
-        int remainder , divs, e, v;
+    private static KeyValues genKeyValues(int d, int w){
+
+        int remainder , divs, e, v, temp, x, y, prevx, prevy;
         ArrayList<Integer> r = new ArrayList<Integer>();
         ArrayList<Integer> vi = new ArrayList<Integer>();
+        x = temp = prevy = 0;
+        y = prevx = 1;
 
         r.add(0,w); // adding R1
         r.add(1,d); // adding R2
@@ -30,71 +41,67 @@ public class RSA {
         vi.add(1,0);
         vi.add(2,0); //needed to enter for loop - later removed
 
-        try {
-            for (int i = 2; r.get(i-1) > 0; i++) {
-                remainder = r.get(i - 2) % r.get(i - 1);
-                divs = r.get(i - 2) / r.get(i - 1);
-                r.add(i, remainder);
-                vi.add(i, divs);
-            }
+        for (int i = 2; r.get(i-1) > 0; i++) {
 
-            //removing the elements no longer needed
-            r.remove(r.size()-1);
-            vi.remove(vi.size()-1);
+            remainder = r.get(i - 2) % r.get(i - 1);
+            divs = r.get(i - 2) / r.get(i - 1);
 
+            temp = x;
+            x = prevx - divs * x;
+            prevx = temp;
 
-            v = vi.get(vi.size() - 2);
+            temp = y;
+            y = prevy - divs * y;
+            prevy = temp;
 
-            //TODO: fix getting value of e
-            //e=r.get(r.size()-2)+(vi.get(vi.size()-2)*w)/d;
-            e=r.get(r.size()-2) + (v * w);
-            e=e/d;
-
-            Key publicKey = new Key(e,n);
-            return publicKey;
-        }catch(Exception NullPointerException){System.out.println("WRONG");}
-        return null;
-    }
-
-    private static Key genPrivateKey(int d, int w){
-
-        int remainder , divs, e, v;
-        ArrayList<Integer> r = new ArrayList<Integer>();
-        ArrayList<Integer> vi = new ArrayList<Integer>();
-
-        r.add(0,w); // adding R1
-        r.add(1,d); // adding R2
-        r.add(2,1); //ignore this basically
-        vi.add(0,0); //filling first index
-        vi.add(1,0); //filling second index
-        vi.add(2,0);//ignore this basically
-
-        try{
-            for (int i = 2; r.get(i-1) > 0; i++) {
-                remainder = r.get(i - 2) % r.get(i - 1);
-                divs = r.get(i - 2) / r.get(i - 1);
-                r.add(i, remainder);
-                vi.add(i, divs);
-            }
-            r.remove(r.size()-1);
-            vi.remove(vi.size()-1);
-            v = vi.get(vi.size() - 2);
-
-
-            Key privateKey = new Key(d,n);
-            return privateKey;
-        }catch(Exception NullPointerException){System.out.println("WRONG");}
-        return null;
-    }
-
-    //TODO: Must fix e first before implementing encrypt method
-    private static void encrypt(int[] ascii){
-        //Plaintext-M    C=Me(mod n)
-        int[] cipher = new int[ascii.length];
-
-        for(int i=0; i<ascii.length; i++){
-
+            r.add(i, remainder);
+            vi.add(i, divs);
         }
+
+        r.remove(r.size()-1);
+        vi.remove(vi.size()-1);
+
+        e=prevy;
+
+        //KeyValues keyValues = new KeyValues(e,d,n);
+        return new KeyValues(e,d,n);
+
+    }
+
+
+    private static int[] encrypt(Key publicKey, int[] asciiPlaintext){
+
+        int[] cipher = new int[asciiPlaintext.length];
+        String binaryE = Integer.toBinaryString(publicKey.partOne);
+
+        System.out.println(binaryE);
+
+        for(int j=0; j<asciiPlaintext.length; j++){
+            int c = 1;
+            for (int i=0; i<binaryE.length(); i++){
+                char bitE = binaryE.charAt(i);
+                c = (c*c)%publicKey.partTwo;
+                if (bitE=='1'){
+                    c = (c*asciiPlaintext[j])%publicKey.partTwo;
+                }
+            }
+            cipher[j] = c;
+        }
+        return cipher;
+    }
+
+    private static double[] decrypt(Key privateKey, int[] ciphertext){
+
+        double[] decrypted = new double[ciphertext.length];
+        double m;
+
+        for(int i=0; i<ciphertext.length; i++){
+            m = Math.pow(ciphertext[i],privateKey.partOne);
+            m = m % privateKey.partTwo;
+            decrypted[i]=m;
+        }
+
+        return decrypted;
     }
 
     private static int[] convertToNum(String plaintext){
@@ -106,23 +113,53 @@ public class RSA {
         return ascii;
     }
 
+    private static void EncDecB(Key publicKey, Key privateKey){
+        int asii=2; //B
+
+        String binaryE = Integer.toBinaryString(publicKey.partOne);
+        System.out.println(binaryE); //correct
+
+        int c = 1;
+        for (int i=0; i<binaryE.length(); i++){
+            char bitE = binaryE.charAt(i);
+            c = c * c;
+            c = c % publicKey.partTwo;
+            if (bitE=='1'){
+                c = (c*2)%publicKey.partTwo;
+            }
+        }
+
+        int cipher = c;
+        System.out.println("");
+        System.out.println(cipher);
+
+        double result;
+//        result = Math.pow(cipher,83);
+//        System.out.println(result);
+//        result = result%privateKey.partTwo;
+//        System.out.println(result);
+        result = Math.pow(14,31);
+        System.out.println(result);
+        result = result%133;
+        System.out.println(result);
+    }
+
     public static void main(String [] args){
 
-        Key pubKey = genPublicKey(d,w);
-        Key priKey = genPrivateKey(d,w);
-        System.out.println(pubKey.partOne + " " + pubKey.partTwo);
-        System.out.println(priKey.partOne + " " + priKey.partTwo);
+        KeyValues keyValues = genKeyValues(d,w);
+        Key publicKey = new Key(keyValues.e,keyValues.n);
+        Key privateKey = new Key(keyValues.d,keyValues.n);
+        System.out.println("Public Key: <" + publicKey.partOne + "," + publicKey.partTwo + ">");
+        System.out.println("Private Key: <" + privateKey.partOne + "," + privateKey.partTwo + ">");
 
-        int[] ascii = convertToNum(plaintext);
+//        int[] asciiPlaintext = convertToNum(plaintext);
+//
+//        int[] ciphertext = encrypt(publicKey, asciiPlaintext);
+//
+//        double[] decrypted = decrypt(privateKey, ciphertext);
 
-        // using 2 as we are encrypting the letter B
-        int result = 2^pubKey.partOne; // pubKey.partOne is e
-        result = result % w;
-        System.out.println(result); // B encrypted
-
-        int result2 = result ^ d;
-        result2 = result2 % n;
-        System.out.println(result2); // B decrypted == 2
+        //testing which is broke, enc or dec
+        EncDecB(publicKey, privateKey);
 
 
 
@@ -130,3 +167,4 @@ public class RSA {
     }
 
 }
+
